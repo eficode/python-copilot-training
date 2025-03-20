3bbd379cda1309a91a69cc3c1a0299e980574dd7
Aleksi Simell <aleksi.simell@eficode.com>
2025-03-20 08:27:03 +0200

    Add sortable table headers with pandas to examlpe app

--- example_app/app.py
+++ example_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import csv
 from io import StringIO
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -21,7 +22,24 @@ def index():
 @app.route('/data')
 def data():
     try:
-        return render_template('data.html', title=csv_title, data=uploaded_data)
+        df = pd.DataFrame(uploaded_data)
+        sort_by = request.args.get('sort_by')
+        order = request.args.get('order', 'asc')
+
+        if sort_by and sort_by in df.columns:
+            df = df.sort_values(by=sort_by, ascending=(order == 'asc'))
+
+        table_html = df.to_html(classes='table table-striped', index=False, border=0)
+        sortable_header = '<tr>' + ''.join(
+            f'<th><a href="?sort_by={col}&order={"desc" if sort_by == col and order == "asc" else "asc"}">{col} '
+            f'{"▲" if sort_by == col and order == "asc" else "▼" if sort_by == col and order == "desc" else ""}</a></th>'
+            for col in df.columns
+        ) + '</tr>'
+        sortable_table_html = table_html.replace(
+            '<thead>' + table_html.split('<thead>')[1].split('</thead>')[0] + '</thead>',
+            f'<thead>{sortable_header}</thead>'
+        )
+        return render_template('data.html', title=csv_title, data=sortable_table_html)
     except NameError:
         return "No data uploaded yet"
 
